8b724dbfc8a7a24c3db336fe124ed7cc1a6176e8
nilsir <nilsir@qq.com>
2020-12-30 11:27:32 +0800

    添加基类

---
+++ src/Application.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the nilsir/pacypay.
+ *
+ * (c) nilsir <nilsir@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Nilsir\Pacypay;
+
+use Pimple\Container;
+
+class Application extends Container
+{
+}
---
+++ src/Foundation/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the nilsir/pacypay.
+ *
+ * (c) nilsir <nilsir@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Nilsir\Pacypay\Foundation;
+
+use Nilsir\Pacypay\Support\Collection;
+
+class Config extends Collection
+{
+}
